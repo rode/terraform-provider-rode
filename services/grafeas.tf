e8a8071fad7e8d0edc7302ad8c517f065a66f317
Alex Ashley <alexashley15@gmail.com>
2021-07-29 21:15:10 -0400

    port-forward

--- services/grafeas.tf
+++ services/grafeas.tf
@@ -1,12 +1,12 @@
 resource "helm_release" "grafeas" {
   name       = "grafeas"
-  namespace = kubernetes_namespace.rode.metadata[0].name
+  namespace  = kubernetes_namespace.rode.metadata[0].name
   chart      = "grafeas-elasticsearch"
   repository = "https://rode.github.io/charts"
   version    = "0.2.0"
   wait       = true
   values = [
-  <<EOF
+    <<EOF
 grafeas:
   elasticsearch:
     url: http://elasticsearch-master:9200
--- services/main.tf
+++ services/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    helm       = {
+    helm = {
       source  = "hashicorp/helm"
       version = "2.2.0"
     }
